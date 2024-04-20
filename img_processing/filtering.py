1f00a1d3913d93e1c2884606a82f93cf55f8ebd9
juanezete <93271186+juanezete@users.noreply.github.com>
2024-04-20 23:33:31 +0200

    Add files via upload

--- img_processing/filtering.py
+++ img_processing/filtering.py
@@ -1,45 +1,75 @@
 import cv2
 import numpy as np
+import time
+import skimage.morphology as morph
+
 
 def filter_and_enhance(image_path):
     # Leer la imagen
+    start_time = time.time()
     original_image = cv2.imread(image_path)
+    print("Tiempo de procesamiento para leer la imagen:", time.time() - start_time, "segundos")
     
     # Convertir la imagen a espacio de color HSV
+    start_time = time.time()
     hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
+    print("Tiempo de procesamiento para convertir la imagen a HSV:", time.time() - start_time, "segundos")
     
     # Definir los límites del color para el trazado grueso de la trayectoria en HSV
     lower_bound = np.array([166, 100, 37])
     upper_bound = np.array([179, 255, 255])
     
     # Filtrar la imagen para resaltar los trazados gruesos de la trayectoria
+    start_time = time.time()
     mask = cv2.inRange(hsv_image, lower_bound, upper_bound)
+    print("Tiempo de procesamiento para filtrar la imagen:", time.time() - start_time, "segundos")
     
     # Aplicar el filtro morfológico para mejorar la forma de los trazados
+    start_time = time.time()
     kernel = np.ones((7,7),np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    print("Tiempo de procesamiento para aplicar el filtro morfológico:", time.time() - start_time, "segundos")
 
     # Aplicar la operación de erosión para adelgazar la línea
+    start_time = time.time()
     eroded_mask = cv2.erode(mask, kernel, iterations=1)
+    print("Tiempo de procesamiento para aplicar la erosión:", time.time() - start_time, "segundos")
     
     # Se aplica binarizacion de la mascara para obtener la imagen resultante
-    result_image = np.ones_like(original_image) * 255
-    result_image[eroded_mask == 0] = 0
-    result_image=cv2.cvtColor(result_image, cv2.COLOR_BGR2GRAY)
+    start_time = time.time()
+    result_image = np.zeros_like(eroded_mask)
+    result_image[eroded_mask > 0] = 255  
+    print("Tiempo de procesamiento para binarizar la máscara y convertir la imagen resultante a escala de grises:", time.time() - start_time, "segundos")
 
-    result_image= cv2.ximgproc.thinning(result_image)
+    start_time = time.time()
+
+    # Aplicar el algoritmo de Thinning 
+
+    #OPCION 1
+    #result_image= cv2.ximgproc.thinning(result_image)
+
+    #OPCION 2
+    result_image=morph.skeletonize(result_image,method='zhang').astype(np.uint8) * 255
+
+    print("Tiempo de procesamiento para adelgazar la imagen:", time.time() - start_time, "segundos")
 
     # Crear una copia de la imagen original para mostrar las partes blancas de la imagen mejorada
+    start_time = time.time()
     overlay_image = original_image.copy()
+    print("Tiempo de procesamiento para copiar la imagen original:", time.time() - start_time, "segundos")
     
     # Reemplazar los píxeles de la copia con los píxeles blancos de la imagen mejorada
+    start_time = time.time()
     overlay_image[result_image == 255] = [255, 255, 255]
+    print("Tiempo de procesamiento para reemplazar los píxeles de la copia:", time.time() - start_time, "segundos")
     
     # Redimensionar las imágenes para que se ajusten a la pantalla
+    start_time = time.time()
     height, width = original_image.shape[:2]
     scale_factor = 0.5
     scaled_original = cv2.resize(original_image, (int(width*scale_factor), int(height*scale_factor)))
     scaled_result = cv2.resize(overlay_image, (int(width*scale_factor), int(height*scale_factor)))
+    print("Tiempo de procesamiento para redimensionar las imágenes:", time.time() - start_time, "segundos")
     
     # Mostrar la imagen original y la imagen resultante
     cv2.imshow('Enhanced Image', scaled_result)
---
+++ img_processing/video_filtering.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+import time
+import skimage.morphology as morph
+
+def filter_and_enhance(video_path):
+    # Abrir el archivo de video
+    cap = cv2.VideoCapture(video_path)
+    
+    # Obtener el ancho, el alto y la velocidad de fotogramas del video
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Crear un objeto VideoWriter para guardar el video resultante
+    out = cv2.VideoWriter('output_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+
+    # Tiempo del último fotograma capturado
+    last_frame_time = 0
+
+    while cap.isOpened():
+        # Leer el tiempo actual
+        current_time = time.time()
+        
+        # Leer el siguiente fotograma del video
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+        
+        # Capturar un fotograma cada 0.1 segundos
+        if current_time - last_frame_time >= 0.1:
+            # Actualizar el tiempo del último fotograma capturado
+            last_frame_time = current_time
+
+            # Convertir el fotograma a espacio de color HSV
+            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            
+            # Definir los límites del color para el trazado grueso de la trayectoria en HSV
+            lower_bound = np.array([166, 100, 37])
+            upper_bound = np.array([179, 255, 255])
+            
+            # Filtrar el fotograma para resaltar los trazados gruesos de la trayectoria
+            mask = cv2.inRange(hsv_frame, lower_bound, upper_bound)
+            
+            # Aplicar el filtro morfológico para mejorar la forma de los trazados
+            kernel = np.ones((7,7),np.uint8)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+            # Aplicar la operación de erosión para adelgazar la línea
+            eroded_mask = cv2.erode(mask, kernel, iterations=1)
+            
+            # Aplicar binarización de la máscara para obtener el fotograma resultante
+            result_frame = np.ones_like(frame) * 255
+            result_frame[eroded_mask == 0] = 0
+            result_frame = cv2.cvtColor(result_frame, cv2.COLOR_BGR2GRAY)
+
+            result_frame=morph.skeletonize(result_frame,method='zhang').astype(np.uint8) * 255
+
+
+            # Crear una copia del fotograma original para mostrar las partes blancas del fotograma mejorado
+            overlay_frame = frame.copy()
+            
+            # Reemplazar los píxeles de la copia con los píxeles blancos del fotograma mejorado
+            overlay_frame[result_frame == 255] = [255, 255, 255]
+            
+            # Escribir el fotograma procesado en el video de salida
+            out.write(overlay_frame)
+
+            # Calcular y mostrar el porcentaje de progreso
+            current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+            percentage = (current_frame / total_frames) * 100
+            print(f'Procesando: {percentage:.2f}% completado', end='\r')
+
+    # Liberar los recursos y cerrar todas las ventanas
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+# Ruta del video
+video_path = 'videaco.mp4'
+
+# Llamar a la función para filtrar y mejorar el video
+filter_and_enhance(video_path)
